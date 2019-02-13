125e656525f34e1af24e0284a033dc2a3b936364
Nathan <lemnd1002@gmail.com>
2019-02-13 17:47:03 +0700

    add event-wedding section

---
+++ flower-wedding/src/js/site.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    let deadline = Date.parse('Jan 16, 2021');
+    initializeClock('clockcountdown', deadline);
+    
+});
+// CountDown
+getTimeRemaining = (endtime) => {
+    let t = endtime - new Date().getTime();
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  initializeClock = (id, endtime) => {
+    let clock = document.getElementById(id);
+    let daysSpan = clock.querySelector('.days');
+    let hoursSpan = clock.querySelector('.hours');
+    let minutesSpan = clock.querySelector('.minutes');
+    let secondsSpan = clock.querySelector('.seconds');
+    updateClock = () => {
+        let t = getTimeRemaining(endtime);
+    
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+      }
+    updateClock();
+    let timeinterval = setInterval(updateClock, 1000);
+  }
+  
