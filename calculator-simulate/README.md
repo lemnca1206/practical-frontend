541451f447eb098325d1b5b18add668499638322
Nathan <lemnca1206@gmail.com>
2019-03-15 09:45:02 +0700

    add calculator project

---
+++ calculator-simulate/README.md
@@ -0,0 +1,12 @@
+###How to use HTML Core
+
+  ##Install
+    - Install Node.js < https://nodejs.org/en/>
+    - Install Gulp < "npm install gulp -g"
+
+  ##Run
+    - Run: "gulp" to start the Website
+    - Build: "gulp project" to build the Website
+
+  ##Option
+    - Library of js or js put into the lib folder and name with numberic to set order for them
---
+++ calculator-simulate/js/main.js
@@ -0,0 +1,97 @@
+$(document).ready(() => {    
+    const keys = $('.calculator-keys');
+    keys.on('click', (event) => {
+        const {target} = event;
+        if (!target.matches('button')) {
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            handleOperator(target.value);
+            updateDisplay();
+            return;
+        }
+    
+        if (target.classList.contains('decimal')) {
+            inputDecimal(target.value);
+            updateDisplay();
+            return;
+        }
+    
+        if (target.classList.contains('all-clear')) {
+            resetCalculator(target.value);
+            updateDisplay()
+            return;
+        }
+        inputDigit(target.value);
+        updateDisplay();
+    })
+    updateDisplay();
+});
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null
+};
+
+updateDisplay = () => {
+    const display = $('.calculator-screen');
+    display.val(calculator.displayValue);
+};
+inputDigit = (digit) => {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
+inputDecimal = (dot) => {
+    if (calculator.waitingForSecondOperand === true) return;
+
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+}
+handleOperator = (nextOperator) => {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+  
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
+  
+    if (firstOperand == null) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const currentValue = firstOperand || 0;
+      const result = performCalculation[operator](currentValue, inputValue);  
+      calculator.displayValue = String(result);
+      calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+  
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+  
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+  
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+  
+    '=': (firstOperand, secondOperand) => secondOperand
+};
+resetCalculator = () => {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
